bd2c220fce7cd0c88f7d5a41aa693f4b3167fc10
pstcl <40257916+pstcl@users.noreply.github.com>
2019-02-13 15:08:42 +0530

    committing after implementing repo methid for search

--- src/main/java/org/pstcl/sldc/scada/repository/ScadaDataEntityRepository.java
+++ src/main/java/org/pstcl/sldc/scada/repository/ScadaDataEntityRepository.java
@@ -1,12 +1,38 @@
 package org.pstcl.sldc.scada.repository;
 
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
 import org.pstcl.sldc.scada.model.ScadaDataEntity;
-import org.pstcl.sldc.scada.model.ScadaEntity;
 import org.pstcl.sldc.scada.model.ScadaEntityId;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @RepositoryRestResource(collectionResourceRel="scadadata",path="scadadata")
-public interface ScadaDataEntityRepository extends CrudRepository<ScadaDataEntity, ScadaEntityId> {
+public interface ScadaDataEntityRepository extends PagingAndSortingRepository<ScadaDataEntity, ScadaEntityId> {
+	
+	
+	@Override
+    @RestResource(exported = false)
+    public List<ScadaDataEntity> findAll();
+	
+	
 
+	@RestResource(exported = false)
+    @Query("select entity from ScadaDataEntity entity where entity.entityId.dateS >= :dateS")
+    List<ScadaDataEntity> findAllWithdateS(
+    		@DateTimeFormat(pattern="yyyy-MM-dd")    		@Param("dateS") LocalDate dateS);
+	//@DateTimeFormat(pattern="yyyy-MM-dd")@Param("dateS")
+
+//    @Query("select entity from ScadaDataEntity entity where entity.entityId.dateS >= :dateS")
+//    List<ScadaDataEntity> findAllWithDdeitem(
+//    		@DateTimeFormat(pattern="yyyy-MM-dd")@Param("dateS") Date dateS);
+//	
+//	
+	
 }
