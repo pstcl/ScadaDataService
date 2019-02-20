8cd16c9f98e7d6087feb3e138c5c9298c5002d5a
Leevansha <leevansha@gmail.com>
2019-02-20 12:39:01 +0530

    uuyguguguy#

--- src/main/java/org/pstcl/sldc/scada/repository/ScadaDataEntityRepository.java
+++ src/main/java/org/pstcl/sldc/scada/repository/ScadaDataEntityRepository.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
+
 import org.pstcl.sldc.scada.model.ScadaDataEntity;
 import org.pstcl.sldc.scada.model.ScadaEntityId;
 import org.springframework.data.jpa.repository.Query;
@@ -24,15 +25,29 @@ public interface ScadaDataEntityRepository extends PagingAndSortingRepository<Sc
 	
 
 	@RestResource(exported = false)
-    @Query("select entity from ScadaDataEntity entity where entity.entityId.dateS >= :dateS")
-    List<ScadaDataEntity> findAllWithdateS(
-    		@DateTimeFormat(pattern="yyyy-MM-dd")    		@Param("dateS") LocalDate dateS);
+    @Query(value="select * from scadadata4 where dateS >= :dateS",nativeQuery=true)
+    List<ScadaDataEntity> findAllWithdateS(@Param("dateS") LocalDate dateS);
 	//@DateTimeFormat(pattern="yyyy-MM-dd")@Param("dateS")
 
-//    @Query("select entity from ScadaDataEntity entity where entity.entityId.dateS >= :dateS")
-//    List<ScadaDataEntity> findAllWithDdeitem(
-//    		@DateTimeFormat(pattern="yyyy-MM-dd")@Param("dateS") Date dateS);
-//	
-//	
+
+	@RestResource(exported = false)
+	@Query(value="SELECT * from scadadata4 where ddeItem = '*/Rate=10'  ORDER BY dateS DESC,timeS DESC LIMIT :limit",nativeQuery=true)
+	List<ScadaDataEntity> findAllWithFrequency(@Param("limit") int limit);
 	
+	
+	@RestResource(exported = false)
+	@Query(value="SELECT * from scadadata4 where ddeItem = 'nrldc_pg.line.pseb_drwl.mw/rate=10'  ORDER BY dateS DESC,timeS DESC LIMIT :limit",nativeQuery=true)
+	List <ScadaDataEntity> findAllDrawal(@Param("limit") int limit);
+	
+	@RestResource(exported = false)
+	@Query(value="SELECT * from scadadata4 where ddeItem = 'nrldc_pg.shdl.pseb_s.mw/rate=10'  ORDER BY dateS DESC,timeS DESC LIMIT :limit",nativeQuery=true)
+	List <ScadaDataEntity> findAllSchedule(@Param("limit") int limit);
+	
+	@RestResource(exported = false)
+	@Query(value="SELECT * from scadadata4 where ddeItem = 'nrldc_pg.line.pseb_od_ud.mw/rate=10'  ORDER BY dateS DESC,timeS DESC LIMIT :limit",nativeQuery=true)
+	List <ScadaDataEntity> findAllOD_UD(@Param("limit") int limit);
+	
+	@RestResource(exported = false)
+	@Query(value="SELECT * from scadadata4 where ddeItem = 'nrldc_pg.ld.pseb_load.mw/rate=10'  ORDER BY dateS DESC,timeS DESC LIMIT :limit",nativeQuery=true)
+	List <ScadaDataEntity> findAllLoad(@Param("limit") int limit);
 }
